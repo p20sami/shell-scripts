5b652d34d5a8433eb94c60bf7ec0aa3bbd575301
p20sami <92584026+p20sami@users.noreply.github.com>
2021-11-21 15:17:22 +0200

    a script in bash for specs

---
+++ specsscript.sh
@@ -0,0 +1,22 @@
+echo " Welcome to a specs script! "
+
+echo " What specs would you like to see? "
+
+echo " a) Software "
+
+echo " b) Hardware "
+
+read specs
+
+if [ $specs = "a" ]
+then
+        neofetch
+        uname
+fi
+
+if [ $specs = "b" ]
+then
+        neofetch
+        lscpu
+        lspci
+fi
